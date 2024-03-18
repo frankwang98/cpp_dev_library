5c896cfdc3314ccca16364d4736472405a94e3c9
wangzf <wangzhifeng@wicri.org>
2024-03-18 14:32:13 +0800

    脚本语言分出

--- script/python/PyFlaskDocker/README.md
+++
@@ -1,9 +0,0 @@
-# docker-compose test
-
-## 运行
-docker-compose up
-
-浏览器打开`localhost:5000`
-
-## 卸载
-docker-compose down
--- script/python/django-example/HelloWorld/HelloWorld/views.py
+++
@@ -1,8 +0,0 @@
-"""
-helloworld
-"""
-
-from django.http import HttpResponse
-
-def hello(request):
-    return HttpResponse("Hello world ! ")
